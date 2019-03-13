96db76ee685799306e6f5c01d65c8e58c72e8bab
Gabriel N Schenker <gnschenker@gmail.com>
2019-03-13 15:10:40 +0300

    Added lab 'creating a kafka consumer'

---
+++ labs/create-consumer/src/main/java/clients/VehiclePositionConsumer.java
@@ -0,0 +1,18 @@
+package clients;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class VehiclePositionConsumer {
+    public static void main(String[] args) {
+        System.out.println("*** Starting VP Consumer ***");
+        
+        // TODO: add code...
+    }
+}

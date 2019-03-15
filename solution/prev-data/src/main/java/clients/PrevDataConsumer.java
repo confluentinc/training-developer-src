45527d6e75b84f8d8dc372b51d8767505e232347
Gabriel N Schenker <gnschenker@gmail.com>
2019-03-15 17:16:45 +0300

    Added various lab solutions

---
+++ solution/prev-data/src/main/java/clients/PrevDataConsumer.java
@@ -0,0 +1,54 @@
+package clients;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class PrevDataConsumer {
+    public static void main(String[] args) {
+        System.out.println("*** Starting Prev Data Consumer ***");
+        
+        Properties settings = new Properties();
+        settings.put(ConsumerConfig.GROUP_ID_CONFIG, "prev-data-consumer");
+        settings.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        settings.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        settings.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(settings);
+
+        ConsumerRebalanceListener listener = new ConsumerRebalanceListener() {
+            @Override
+			public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                // nothing to do...
+			}
+            
+            @Override
+			public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+				consumer.seekToBeginning(partitions);
+			}
+        };
+        
+        try {
+            consumer.subscribe(Arrays.asList("vehicle-positions"), listener);
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<String, String> record : records)
+                    System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+            }
+        }
+        finally{
+            System.out.println("*** Ending Prev Data Consumer ***");
+            consumer.close();
+        }
+    }
+}

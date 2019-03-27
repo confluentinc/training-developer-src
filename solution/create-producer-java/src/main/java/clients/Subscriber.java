3dc6d68aaee548839ca9189d1c4b422f71028cd0
Gabriel N Schenker <gnschenker@gmail.com>
2019-03-27 10:19:48 +0300

    Added lab for Java producer

--- solution/create-producer-java/src/main/java/clients/Subscriber.java
+++ solution/create-producer-java/src/main/java/clients/Subscriber.java
@@ -1,30 +1,34 @@
 package clients;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class Subscriber implements MqttCallback {
 
     private final int qos = 1;
-    private String topic = "/hfp/v1/journey/ongoing/+/+/+/+/+/+/+/+/+/#";
+    private String host = "ssl://mqtt.hsl.fi:8883";
+    private String clientId = "MQTT-Java-Example";
+    private String topic = "/hfp/v1/journey/ongoing/#";
+    private String kafka_topic = "vehicle-positions";
     private MqttClient client;
 
-    public Subscriber() throws MqttException {
-        // String host = "mqtts://mqtt.hsl.fi:8883";
-        String host = "tcp://mqtt.hsl.fi:8883";
-        String clientId = "MQTT-Java-Example";
+    private KafkaProducer<String, String> producer;
 
+    public Subscriber(KafkaProducer<String, String> producer) {
+        this.producer = producer;
+    }
+
+    public void start() throws MqttException {
         MqttConnectOptions conOpt = new MqttConnectOptions();
         conOpt.setCleanSession(true);
 
-        // this.client = new MqttClient(host, clientId, new MemoryPersistence());
-        // this.client.setCallback(this);
-        // this.client.connect(conOpt);
-        MqttClient client = new MqttClient(host, MqttClient.generateClientId());
-        client.setCallback( this );
-        client.connect();
+        this.client = new MqttClient(this.host, this.clientId, new MemoryPersistence());
+        this.client.setCallback(this);
+        this.client.connect(conOpt);
         
-        this.client.subscribe(this.topic, qos);
+        this.client.subscribe(this.topic, this.qos);
     }
 
     public void connectionLost(Throwable cause) {
@@ -37,5 +41,9 @@ public class Subscriber implements MqttCallback {
 
     public void messageArrived(String topic, MqttMessage message) throws MqttException {
         System.out.println(String.format("[%s] %s", topic, new String(message.getPayload())));
+        final String key = topic;
+        final String value = new String(message.getPayload());
+        final ProducerRecord<String, String> record = new ProducerRecord<>(this.kafka_topic, key, value);
+        producer.send(record);
     }
 }

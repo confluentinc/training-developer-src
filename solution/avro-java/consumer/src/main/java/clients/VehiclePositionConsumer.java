d475f402c47a19b2d7f4ea5263519aa1220c8a9e
Gabriel N Schenker <gnschenker@gmail.com>
2019-04-29 11:28:55 +0200

    Added lab Kafka Avro consumer

--- solution/avro-java/consumer/src/main/java/clients/VehiclePositionConsumer.java
+++ solution/avro-java/consumer/src/main/java/clients/VehiclePositionConsumer.java
@@ -7,31 +7,37 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import solution.model.PositionKey;
+import solution.model.PositionValue;
 
 public class VehiclePositionConsumer {
     public static void main(String[] args) {
-        System.out.println("*** Starting VP Consumer ***");
+        System.out.println("*** Starting VP Consumer Avro ***");
         
         Properties settings = new Properties();
-        settings.put(ConsumerConfig.GROUP_ID_CONFIG, "vp-consumer");
+        settings.put(ConsumerConfig.GROUP_ID_CONFIG, "vp-consumer-avro");
         settings.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
         settings.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        settings.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        settings.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        settings.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        settings.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        settings.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, "true");
+        settings.put("schema.registry.url", "http://schema-registry:8081");
 
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(settings);
+        KafkaConsumer<PositionKey, PositionValue> consumer = new KafkaConsumer<>(settings);
         try {
-            consumer.subscribe(Arrays.asList("vehicle-positions"));
+            consumer.subscribe(Arrays.asList("vehicle-positions-avro"));
 
             while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                for (ConsumerRecord<String, String> record : records)
-                    System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+                ConsumerRecords<PositionKey, PositionValue> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<PositionKey, PositionValue> record : records)
+                    System.out.printf("offset = %d, key = %s, value = %s\n", 
+                        record.offset(), record.key().toString(), record.value().toString());
             }
         }
         finally{
-            System.out.println("*** Ending VP Consumer ***");
+            System.out.println("*** Ending VP Consumer Avro ***");
             consumer.close();
         }
     }

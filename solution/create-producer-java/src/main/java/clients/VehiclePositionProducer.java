1e88cb651716cb57c17ae6bb23493ec4c9f5ba54
Gabriel N Schenker <gnschenker@gmail.com>
2019-06-04 16:08:41 +0300

    Addresses issue #299

--- solution/create-producer-java/src/main/java/clients/VehiclePositionProducer.java
+++ solution/create-producer-java/src/main/java/clients/VehiclePositionProducer.java
@@ -1,7 +1,10 @@
 package clients;
 
 import java.util.Properties;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.eclipse.paho.client.mqttv3.*;
 
 public class VehiclePositionProducer {
@@ -9,10 +12,10 @@ public class VehiclePositionProducer {
         System.out.println("*** Starting VP Producer ***");
 
         Properties settings = new Properties();
-        settings.put("client.id", "vp-producer");
-        settings.put("bootstrap.servers", "kafka:9092");
-        settings.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        settings.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        settings.put(ProducerConfig.CLIENT_ID_CONFIG, "vp-producer");
+        settings.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        settings.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        settings.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         final KafkaProducer<String, String> producer = new KafkaProducer<>(settings);
         

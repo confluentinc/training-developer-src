6a3bea3a7933f94f997cc2ba5a73c41f2e464f0a
Gabriel N Schenker <gnschenker@gmail.com>
2019-06-04 16:08:41 +0300

    Addresses issue #299

---
+++ labs/create-consumer-net/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Threading;
+using Confluent.Kafka;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Starting consumer (.NET)");
+        // TODO: Add code here
+    }
+}
